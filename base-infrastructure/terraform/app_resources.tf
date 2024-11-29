f1ba1c656a1e3ec1766fddda40bc81f7a4254d57
Moses Anzagi <moses@datafriendlyspace.org>
2024-11-29 08:07:52 +0300

    update alerthub appresources module

--- base-infrastructure/terraform/app_resources.tf
+++ base-infrastructure/terraform/app_resources.tf
@@ -15,23 +15,31 @@ module "risk_module_resources" {
 module "alert_hub_resources" {
   source = "./app_resources"
 
+  app_name            = "alert-hub"
+  environment         = var.environment
+  resource_group_name = module.resources.resource_group
+
   aks_config = {
     cluster_namespace       = "alert-hub"
     cluster_oidc_issuer_url = module.resources.cluster_oidc_issuer_url
     service_account_name    = "service-token-reader"
   }
 
+#  database_config = {
+#    create_database = true
+#    database_name   = <database_name>
+#    server_id       = <server_id>
+#  }
+
   storage_config = {
     enabled              = true
+
+    container_refs = [
+      "media",
+      "static"
+    ]
+
     storage_account_id   = module.resources.storage_account_id
     storage_account_name = module.resources.storage_account_name
-    container_refs = [
-#      "media",
-#      "static"
-    ]
   }
-
-  app_name            = "alert-hub"
-  environment         = var.environment
-  resource_group_name = module.resources.resource_group
 }
