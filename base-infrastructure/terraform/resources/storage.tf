279a3041e51e1cb4b336749dd5ca4ad7eab5bf2f
Moses Anzagi <moses@datafriendlyspace.org>
2025-02-28 08:38:33 +0300

    create storage accounts for sdt and montandon applications

--- base-infrastructure/terraform/resources/storage.tf
+++ base-infrastructure/terraform/resources/storage.tf
@@ -11,3 +11,29 @@ resource "azurerm_storage_container" "data" {
   storage_account_name  = azurerm_storage_account.ifrcgo.name
   container_access_type = "private"
 }
+
+resource "random_integer" "sdt_storage_account_suffix" {
+  min = 1000
+  max = 9999
+}
+
+resource "azurerm_storage_account" "sdt" {
+  name                     = "sdt${var.environment}storage${random_integer.sdt_storage_account_suffix.result}"
+  resource_group_name      = data.azurerm_resource_group.ifrcgo.name
+  location                 = data.azurerm_resource_group.ifrcgo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_integer" "montandon_storage_account_suffix" {
+  min = 1000
+  max = 9999
+}
+
+resource "azurerm_storage_account" "montandon" {
+  name                     = "montandon${var.environment}storage${random_integer.montandon_storage_account_suffix.result}"
+  resource_group_name      = data.azurerm_resource_group.ifrcgo.name
+  location                 = data.azurerm_resource_group.ifrcgo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
