5c198f18562c9540f19c167e43c54aacfeae2a78
thenav56 <ayernavin@gmail.com>
2025-03-27 12:12:54 +0545

    Manual formatting fixes

--- base-infrastructure/terraform/resources/aks.tf
+++ base-infrastructure/terraform/resources/aks.tf
@@ -53,10 +53,10 @@ resource "azurerm_role_assignment" "storage" {
 # create k8s configmaps and secrets
 
 provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.ifrcgo.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.ifrcgo.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.ifrcgo.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.ifrcgo.kube_config.0.cluster_ca_certificate)
+  host                   = azurerm_kubernetes_cluster.ifrcgo.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.ifrcgo.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.ifrcgo.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.ifrcgo.kube_config[0].cluster_ca_certificate)
 }
 
 # This ConfigMap stores configurations for resources created by Terraform which 
--- base-infrastructure/terraform/resources/database.tf
+++ base-infrastructure/terraform/resources/database.tf
@@ -1,3 +1,4 @@
+# tflint-ignore: terraform_unused_declarations
 data "azurerm_postgresql_flexible_server" "ifrcgo" {
   name                = var.environment == "staging" ? var.ifrcgo_test_resources_db_server : var.ifrcgo_prod_resources_db_server
   resource_group_name = data.azurerm_resource_group.ifrcgo.name
