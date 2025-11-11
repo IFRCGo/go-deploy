686460beee76016cb8d7530de4ee6e65306f1a5a
thenav56 <ayernavin@gmail.com>
2025-11-11 14:08:51 +0545

    feat(k8s): add rabbitmq operator

---
+++ base-infrastructure/terraform/resources/helm-rabbitmq-operator.tf
@@ -0,0 +1,12 @@
+# NOTE: It is installed in rabbitmq-system
+resource "helm_release" "rabbitmq-cluster-operator" {
+  name        = "rabbitmq-cluster-operator"
+  max_history = 10
+
+  chart   = "oci://ghcr.io/toggle-corp/helm-charts/rabbitmq-cluster-operator"
+  version = "v2.17.0"
+
+  depends_on = [
+    azurerm_kubernetes_cluster.ifrcgo
+  ]
+}
