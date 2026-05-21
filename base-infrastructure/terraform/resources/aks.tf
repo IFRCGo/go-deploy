18773d5749220c1281c1fcb7bec60bf6e2b752c6
Szabo Zoltan <szabozoltan69@gmail.com>
2026-05-21 10:38:55 +0200

    Increase AKS default node pool size to D8s v5

--- base-infrastructure/terraform/resources/aks.tf
+++ base-infrastructure/terraform/resources/aks.tf
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "ifrcgo" {
 
   default_node_pool {
     name                        = "nodepool1"
-    vm_size                     = "Standard_DS3_v2"
+    vm_size                     = "Standard_D8s_v5"
     vnet_subnet_id              = azurerm_subnet.aks.id
     enable_auto_scaling         = true
     min_count                   = 1
